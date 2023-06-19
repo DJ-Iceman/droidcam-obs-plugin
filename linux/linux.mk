d510c3b0b99ed3b246f65c40433521155a8a5cc4
aramg <aramg@users.noreply.github.com>
2023-06-19 13:59:48 -0700

    Update Linux makefiles

--- linux/linux.mk
+++ linux/linux.mk
@@ -4,7 +4,6 @@
 # Allow static linking some deps
 ALLOW_STATIC ?= no
 
-# todo: use pkg-config
 # libimobiledevice
 IMOBILEDEV_DIR ?= /opt/libimobiledevice
 IMOBILEDEV_LIB ?= $(IMOBILEDEV_DIR)/lib
@@ -13,25 +12,32 @@ IMOBILEDEV_LIB ?= $(IMOBILEDEV_DIR)/lib
 JPEG_DIR ?= /opt/libjpeg-turbo
 JPEG_LIB ?= $(JPEG_DIR)/lib$(shell getconf LONG_BIT)
 
+define ADD_Lib =
+	INCLUDES += $(shell pkg-config --cflags-only-I  $(1))
+	LDD_LIBS += $(shell pkg-config --libs-only-l  $(1))
+endef
+
 ifeq "$(ALLOW_STATIC)" "yes"
+	INCLUDES += -I$(IMOBILEDEV_DIR)/include
+	INCLUDES += -I$(JPEG_DIR)/include
+
 	STATIC += $(JPEG_LIB)/libturbojpeg.a
 	STATIC += $(IMOBILEDEV_LIB)/libimobiledevice.a
 	STATIC += $(IMOBILEDEV_LIB)/libusbmuxd.a
 	STATIC += $(IMOBILEDEV_LIB)/libplist-2.0.a
 
 else
-	LDD_DIRS += -L$(JPEG_LIB)
-	LDD_DIRS += -L$(IMOBILEDEV_LIB)
-
-	LDD_LIBS += -lturbojpeg
-	LDD_LIBS += -limobiledevice
+$(eval	$(call ADD_Lib,libturbojpeg))
+$(eval	$(call ADD_Lib,libusbmuxd))
 endif
 
-INCLUDES += -I$(IMOBILEDEV_DIR)/include
-INCLUDES += -I$(JPEG_DIR)/include
+ifdef DROIDCAM_OVERRIDE
+CXXFLAGS += -DDROIDCAM_OVERRIDE=1
+include linux/gui.mk
+endif
+
 INCLUDES += -I/usr/include/ffmpeg
 INCLUDES += -I/usr/include/obs
 
 LDD_LIBS += -lobs
-
 LDD_FLAG += -shared
